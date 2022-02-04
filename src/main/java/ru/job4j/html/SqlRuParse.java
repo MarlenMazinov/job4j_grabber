6d674966b066a5f1cd126c581202d13a12d4bc21
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-02-04 21:20:41 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,28 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+        }
+        row = doc.select(".altCol");
+        Set<String> set = new LinkedHashSet<>();
+        for (Element td : row) {
+            Element parent = td.parent();
+            set.add(parent.child(5).text());
+        }
+        set.forEach(System.out::println);
+    }
+}
