03eac3186c48612b3daa138f2cabc29e94cba243
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-02-06 20:08:12 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,7 +8,16 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select("tr");
+        getInformation(doc);
+        Elements pages = doc.select("td:contains(Страницы)").get(0).children();
+        for (int i = 1; i < 5; i++) {
+            doc = Jsoup.connect(pages.get(i).attr("href")).get();
+            getInformation(doc);
+        }
+    }
+
+    private static void getInformation(Document document) {
+        Elements row = document.select("tr");
         for (Element tr : row) {
             if (tr.children().hasClass("postslisttopic")) {
                 System.out.println(tr.child(1).text());
