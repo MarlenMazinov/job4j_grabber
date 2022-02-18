3b300ae503e9d7e0069f9ceb74b851b0cd3c0e17
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-02-18 17:51:24 +0300

    1. Демонстрация работы GC [#1589]

---
+++ src/main/java/ru/job4j/User.java
@@ -0,0 +1,42 @@
+package ru.job4j;
+
+public class User {
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %d %s%n", id, name);
+    }
+}
+
+class GCDemo {
+
+    private static final long KB = 1000;
+    private static final long MB = KB * KB;
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
+    public static void info() {
+        final long freeMemory = ENVIRONMENT.freeMemory();
+        final long totalMemory = ENVIRONMENT.totalMemory();
+        final long maxMemory = ENVIRONMENT.maxMemory();
+        System.out.println("=== Environment state ===");
+        System.out.printf("Free: %d%n", freeMemory / MB);
+        System.out.printf("Total: %d%n", totalMemory / MB);
+        System.out.printf("Max: %d%n", maxMemory / MB);
+    }
+
+    public static void main(String[] args) {
+        info();
+        for (int i = 0; i < 10000; i++) {
+            new User(i, "N" + i);
+        }
+        info();
+    }
+
+}
