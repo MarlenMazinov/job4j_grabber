518b3ae135ffa845d301f8f45cf0fa5392ab7a1f
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-03-01 19:49:09 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.template;
+
+import junit.framework.TestCase;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeneratorTest extends TestCase {
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExcessKeys() {
+        Generator generator = new SomeGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("age", "36");
+        generator.produce("My name is ${name}", map);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoKeysInMap() {
+        Generator generator = new SomeGenerator();
+        Map<String, String> map = new HashMap<>();
+        generator.produce("My name is ${name}", map);
+    }
+}
