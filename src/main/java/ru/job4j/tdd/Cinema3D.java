cf9a33e8bb6d72f6c73aee589c246ec13c608e8a
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-03-02 16:00:21 +0300

    2. Что такое TDD? [#4918 #270602]

--- src/main/java/ru/job4j/tdd/Cinema3D.java
+++ src/main/java/ru/job4j/tdd/Cinema3D.java
@@ -1,67 +1,22 @@
 package ru.job4j.tdd;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Cinema3D implements Cinema {
-    private List<Session> sessions = new ArrayList<>();
-    private int[][] places = new int[10][20];
-    private List<Calendar> freeDates;
-
-    public List<Session> getSessions() {
-        return sessions;
-    }
-
-    public void setSessions(List<Session> sessions) {
-        this.sessions = sessions;
-    }
-
-    public int[][] getPlaces() {
-        return places;
-    }
-
-    public void setPlaces(int[][] places) {
-        this.places = places;
-    }
-
-    public List<Calendar> getFreeDates() {
-        return freeDates;
-    }
-
-    public void setFreeDates(List<Calendar> freeDates) {
-        this.freeDates = freeDates;
-    }
 
     @Override
     public List<Session> find(Predicate<Session> filter) {
-        return List.of(new Session3D());
+        return null;
     }
 
     @Override
     public Ticket buy(Account account, int row, int column, Calendar date) {
-        Ticket rsl = null;
-        if (row > 10 || column > 20) {
-            throw new IllegalArgumentException("Места с таким номером не существует");
-        }
-        if (places[row][column] != 0) {
-            throw new IllegalArgumentException("Данное место продано");
-        }
-
-        for (Calendar elem : freeDates) {
-            elem.set(Calendar.MILLISECOND, 00);
-            date.set(Calendar.MILLISECOND, 00);
-            if (elem.equals(date)) {
-                places[row][column] = 1;
-                rsl = new Ticket3D(account, row, column, date);
-            }
-        }
-        return rsl;
+        return null;
     }
 
     @Override
     public void add(Session session) {
-        sessions.add(session);
     }
 }
--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -2,101 +2,80 @@ package ru.job4j.tdd;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
 public class CinemaTest {
 
+    @Ignore
     @Test
     public void whenBuySuccessful() {
         Account account = new AccountCinema();
-        Cinema3D cinema = new Cinema3D();
-        List<Calendar> dates = new ArrayList<>();
-        Calendar freeDate = Calendar.getInstance();
-        freeDate.set(2020, 10, 10, 23, 00, 00);
-        dates.add(freeDate);
-        cinema.setFreeDates(dates);
-        Calendar boughtDate = Calendar.getInstance();
-        boughtDate.set(2020, 10, 10, 23, 00, 00);
-        Ticket ticket = cinema.buy(account, 1, 1, boughtDate);
-        assertEquals(ticket, new Ticket3D(account, 1, 1, boughtDate));
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertEquals(ticket, new Ticket3D());
     }
 
+    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void whenBuyWrongDate() {
         Account account = new AccountCinema();
-        Cinema3D cinema = new Cinema3D();
-        List<Calendar> dates = new ArrayList<>();
-        Calendar freeDate = Calendar.getInstance();
-        freeDate.set(2020, 10, 10, 23, 00, 00);
-        dates.add(freeDate);
-        cinema.setFreeDates(dates);
-        Calendar boughtDate = Calendar.getInstance();
-        boughtDate.set(2020, 10, 10, 21, 00, 00);
-        Ticket ticket = cinema.buy(account, 12, 1, boughtDate);
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
     }
 
+    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void whenBuyWrongRow() {
         Account account = new AccountCinema();
-        Cinema3D cinema = new Cinema3D();
-        List<Calendar> dates = new ArrayList<>();
-        Calendar freeDate = Calendar.getInstance();
-        freeDate.set(2020, 10, 10, 23, 00, 00);
-        dates.add(freeDate);
-        cinema.setFreeDates(dates);
-        Calendar boughtDate = Calendar.getInstance();
-        boughtDate.set(2020, 10, 10, 23, 00, 00);
-        Ticket ticket = cinema.buy(account, 12, 1, boughtDate);
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
     }
 
+    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void whenBuyWrongColumn() {
         Account account = new AccountCinema();
-        Cinema3D cinema = new Cinema3D();
-        List<Calendar> dates = new ArrayList<>();
-        Calendar freeDate = Calendar.getInstance();
-        freeDate.set(2020, 10, 10, 23, 00, 00);
-        dates.add(freeDate);
-        cinema.setFreeDates(dates);
-        Calendar boughtDate = Calendar.getInstance();
-        boughtDate.set(2020, 10, 10, 23, 00, 00);
-        Ticket ticket = cinema.buy(account, 10, 25, boughtDate);
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
     }
 
+    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void whenBuyBusyPlace() {
         Account account = new AccountCinema();
-        Cinema3D cinema = new Cinema3D();
-        List<Calendar> dates = new ArrayList<>();
-        Calendar freeDate = Calendar.getInstance();
-        freeDate.set(2020, 10, 10, 23, 00, 00);
-        dates.add(freeDate);
-        cinema.setFreeDates(dates);
-        int[][] arr = new int[10][20];
-        arr[5][10] = 1;
-        cinema.setPlaces(arr);
-        Calendar boughtDate = Calendar.getInstance();
-        boughtDate.set(2020, 10, 10, 23, 00, 00);
-        Ticket ticket = cinema.buy(account, 5, 10, boughtDate);
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
     }
 
+    @Ignore
     @Test
     public void whenFind() {
-        Cinema3D cinema = new Cinema3D();
-        cinema.setSessions(List.of(new Session3D()));
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
         assertEquals(sessions, Arrays.asList(new Session3D()));
     }
 
+    @Ignore
     @Test
     public void whenAdd() {
-        Cinema3D cinema = new Cinema3D();
+        Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
-        assertEquals(cinema.getSessions().get(0), new Session3D());
+        assertEquals(cinema.find(session -> true).get(0), new Session3D());
     }
 }
