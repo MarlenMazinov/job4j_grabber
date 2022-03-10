8f029cc3fe522ba44377c8047fc575370a8feafa
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-03-10 12:55:52 +0300

    0. Принцип открытости закрытости [#4914]

---
+++ src/main/java/ru/job4j/design/ocp/SomeClass.java
@@ -0,0 +1,20 @@
+package ru.job4j.design.ocp;
+/*
+Некоторый класс SomeClass, экземпляры которого выполняют сохранение информации.
+Приведенный ниже метод save(String information) принимает в качестве параметра объект
+типа String. Если понадобится сохранить объект содержимое БД или изображение,
+то данный метод придется изменить. Поэтому праильно будет сделать реализацию этого метода
+с исспользованием параметра:
+
+public void save(Т information) {
+        //some code that save information
+}
+*/
+
+public class SomeClass {
+    public void save(String information) {
+        /*
+        some code that save information
+         */
+    }
+}
