1a22a635dbcdb41ed20d283d7d5030bb9d5ab956
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-03-11 13:09:44 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851]

--- src/test/java/ru/job4j/design/srp/ReportForDevDepartmentToXmlTest.java
+++ src/test/java/ru/job4j/design/srp/ReportForDevDepartmentToXmlTest.java
@@ -13,16 +13,15 @@ public class ReportForDevDepartmentToXmlTest {
     public void whenReportIsCorrect() throws JAXBException {
         MemStore store = new MemStore();
         Calendar date = Calendar.getInstance();
-        date.set(2020, 10, 25, 23, 00, 00);
-        date.setTimeInMillis(0);
+        date.setTimeInMillis(1606334400020L);
         Employee worker = new Employee("Ivan", date, date, 100);
         store.add(worker);
         Report report = new ReportForDevDepartmentToXml(store);
         String expect = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
                 + "<employers>\n"
                 + "    <employee>\n"
-                + "        <fired>1970-01-01T03:00:00+03:00</fired>\n"
-                + "        <hired>1970-01-01T03:00:00+03:00</hired>\n"
+                + "        <fired>2020-11-25T23:00:00.020+03:00</fired>\n"
+                + "        <hired>2020-11-25T23:00:00.020+03:00</hired>\n"
                 + "        <name>Ivan</name>\n"
                 + "        <salary>100.0</salary>\n"
                 + "    </employee>\n"
