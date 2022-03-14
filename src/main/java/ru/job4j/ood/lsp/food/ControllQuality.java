4756bc5170bfe38599324ece763909af6a16dd82
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-03-14 14:00:46 +0300

    1. Хранилище продуктов [#852 #274618]

--- src/main/java/ru/job4j/ood/lsp/food/ControllQuality.java
+++ src/main/java/ru/job4j/ood/lsp/food/ControllQuality.java
@@ -33,58 +33,7 @@ public class ControllQuality {
         });
     }
 
-    public void qualify(List<Food> products) {
+    public void qualify() {
         products.forEach(product -> distribute(product, stores));
     }
-
-    /*private WarehouseQualifier warehouseQualifier = new WarehouseQualifier();
-    private ShopQualifier shopQualifier = new ShopQualifier();
-    private TrashQualifier trashQualifier = new TrashQualifier();
-
-
-    public ControllQuality(List<Food> list) {
-        this.list = list;
-    }
-
-    public List<Food> getList() {
-        return list;
-    }
-
-    public void setList(List<Food> list) {
-        this.list = list;
-    }
-
-    public Calendar getNow() {
-        return now;
-    }
-
-    public void qualify() {
-        Food food;
-        int counter = 0;
-        while (counter < list.size()) {
-            food = list.get(counter);
-            double timeToExpiration = ((double) (food.getExpiryDate().getTimeInMillis()
-                    - now.getTimeInMillis())) / ((double) (food.getExpiryDate().getTimeInMillis()
-                    - food.getCreateDate().getTimeInMillis()));
-            if (timeToExpiration > 0.75) {
-                warehouseQualifier.move(food);
-                counter++;
-                continue;
-            }
-            if (timeToExpiration > 0.25) {
-                shopQualifier.move(food);
-                counter++;
-                continue;
-            }
-            if (timeToExpiration > 0.001) {
-                food.setDiscount(0.5f);
-                shopQualifier.move(food);
-                counter++;
-                continue;
-            }
-            trashQualifier.move(food);
-            counter++;
-        }
-        list = null;
-    }*/
 }
--- src/test/java/ru/job4j/ood/lsp/food/ControllQualityTest.java
+++ src/test/java/ru/job4j/ood/lsp/food/ControllQualityTest.java
@@ -18,7 +18,7 @@ public class ControllQualityTest {
         Food milk = new Milk("Milk", milkCreate, milkExp, 100f, 0f);
         products.add(milk);
         ControllQuality controllQuality = new ControllQuality(products);
-        controllQuality.qualify(products);
+        controllQuality.qualify();
         int index = controllQuality.getStores().get(0).getProducts().indexOf(milk);
         assertEquals(milk, controllQuality.getStores().get(0).getProducts().get(0));
     }
@@ -33,7 +33,7 @@ public class ControllQualityTest {
         Food milk = new Milk("Milk", milkCreate, milkExp, 100f, 0f);
         products.add(milk);
         ControllQuality controllQuality = new ControllQuality(products);
-        controllQuality.qualify(products);
+        controllQuality.qualify();
         int index = controllQuality.getStores().get(1).getProducts().indexOf(milk);
         assertEquals(milk, controllQuality.getStores().get(1).getProducts().get(index));
     }
@@ -48,7 +48,7 @@ public class ControllQualityTest {
         Food milk = new Milk("Milk", milkCreate, milkExp, 100f, 0f);
         products.add(milk);
         ControllQuality controllQuality = new ControllQuality(products);
-        controllQuality.qualify(products);
+        controllQuality.qualify();
         int index = controllQuality.getStores().get(1).getProducts().indexOf(milk);
         assertEquals(0.5f,
                 controllQuality.getStores().get(1).getProducts().get(index).getDiscount(), 0.001);
@@ -63,7 +63,7 @@ public class ControllQualityTest {
         Calendar milkExp = controllQuality.getNow();
         Food milk = new Milk("Milk", milkCreate, milkExp, 100f, 0f);
         products.add(milk);
-        controllQuality.qualify(products);
+        controllQuality.qualify();
         int index = controllQuality.getStores().get(2).getProducts().indexOf(milk);
         assertEquals(milk, controllQuality.getStores().get(2).getProducts().get(index));
     }
